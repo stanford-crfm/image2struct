3deb54539e7f65c875fb552d50ac95ad33e110be
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-13 18:35:06 -0800

    Update torch version and add installs to github action

--- install-dev.sh
+++ install-dev.sh
@@ -7,17 +7,15 @@ set -e
 sudo sh ./installs/install-latex.sh
 sudo sh ./installs/install-webpage.sh
 
-# # Install python dependencies
-# # On Mac OS, skip installing pytorch with CUDA because CUDA is not supported
-# if [[ $OSTYPE != 'darwin'* ]]; then
-#   # Manually install pytorch with `--no-cache-dir` to avoid pip getting killed: https://stackoverflow.com/a/54329850
-#   pip install torch==2.0.1 torchvision==0.16.2 --index-url https://download.pytorch.org/whl/cu121 --no-cache-dir
-# fi
-# # Install all pinned dependencies
-# pip install -r requirements.txt
-# # upgrade pip to install in edit mode without setup.py
-# pip install --upgrade pip
-# # Install HELM in edit mode
-# pip install -e ".[all,dev]"
-# # Check dependencies
-# pip check
+# Install python dependencies
+# On Mac OS, skip installing pytorch with CUDA because CUDA is not supported
+if [[ $OSTYPE != 'darwin'* ]]; then
+  # Manually install pytorch with `--no-cache-dir` to avoid pip getting killed: https://stackoverflow.com/a/54329850
+  pip install torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cu117 --no-cache-dir
+fi
+# upgrade pip to install in edit mode without setup.py
+pip install --upgrade pip
+# Install HELM in edit mode
+pip install -e ".[all,dev]"
+# Check dependencies
+pip check
