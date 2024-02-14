c35d11936ec5b395539fcf94588d9a1f7fae797a
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-13 19:06:20 -0800

    Add requirements.txt

--- install-dev.sh
+++ install-dev.sh
@@ -12,6 +12,8 @@ if [[ $OSTYPE != 'darwin'* ]]; then
   # Manually install pytorch with `--no-cache-dir` to avoid pip getting killed: https://stackoverflow.com/a/54329850
   pip install torch==2.0.1 torchvision==0.15.2 --index-url https://download.pytorch.org/whl/cu117 --no-cache-dir
 fi
+# Install all pinned dependencies
+pip install -r requirements.txt
 # upgrade pip to install in edit mode without setup.py
 pip install --upgrade pip
 # Install HELM in edit mode
