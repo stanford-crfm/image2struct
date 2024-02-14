7f8a7a33dc31ce6ef84092bd506686330927b411
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-14 11:18:20 -0800

    Added repo filter and associated tests

--- src/image2structure/filter/file_filters/file_filter.py
+++ src/image2structure/filter/file_filters/file_filter.py
@@ -1,11 +1,18 @@
 from abc import ABC, abstractmethod
+from typing import Any, Dict, Tuple
+
+from image2structure.filter.utils import FilterError
+
+
+class FileFilterError(FilterError):
+    pass
 
 
 class FileFilter(ABC):
     """This class is responsible for filtering files."""
 
     @abstractmethod
-    def filter(self, file_path: str) -> bool:
+    def filter(self, file_path: str) -> Tuple[bool, Dict[str, Any]]:
         """Check if the file meets the requirements.
 
         Args:
@@ -13,5 +20,9 @@ class FileFilter(ABC):
 
         Returns:
             bool: True if the file meets the requirements, False otherwise.
+            Dict[str, Any]: Additional information about the file.
+
+        Raises:
+            FileFilterError: If the filtering cannot be performed.
         """
         pass
