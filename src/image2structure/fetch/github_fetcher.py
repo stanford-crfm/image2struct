1dfab2fac08067348870b6298e438448b5be7abb
JosselinSomervilleRoberts <josselin.somerville@gmail.com>
2024-02-21 08:35:58 -0800

    Fix internal dates github fetcher

--- src/image2structure/fetch/github_fetcher.py
+++ src/image2structure/fetch/github_fetcher.py
@@ -63,9 +63,8 @@ class GitHubFetcher(Fetcher):
         query_parameters = {
             "size": f"<={self._max_size_kb}",
             "created": (
-                f">={self._date_created_after.strftime('%Y-%m-%d')}"
-                if self._date_created_before is None
-                else f"{self._date_created_after.strftime('%Y-%m-%d')}..{self._date_created_before.strftime('%Y-%m-%d')}"  # noqa: E501
+                f"{self._date_created_after_internal.strftime('%Y-%m-%d')}.."
+                f"{self._date_created_before_internal.strftime('%Y-%m-%d')}"
             ),
         }
         if self._language.lower() not in [
